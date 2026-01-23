bad6908ff8bad68df099340514253a8da381ec59
joao-ferrot <jpedrofsilva663@hotmail.com>
2026-01-23 20:24:30 -0300

    segundo commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/register',async(req,res)=>{
     const hashedPassword=await bcrypt.hash(password,10);
 
     try{
-        await db.query('INSERT INTO users (noem,email,password) VALUES ($1$,$2)'[nome,email,hashedPassword]);
+        await db.query('INSERT INTO users (nome,email,password) VALUES ($1$,$2)'[nome,email,hashedPassword]);
         res.redirect('/login.html');
 
     }
@@ -32,4 +32,43 @@ app.post('/register',async(req,res)=>{
 
 
     }
-})
+});
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body;
+    try{
+        const result =await db.query('SELECT * FROM users WHERE email=$1',[email]);
+        const user=result.rows[0];
+
+        if(result.rowCount===0){
+            return res.send('usuario nao encontrado');
+        }
+    
+        const valid = await bcrypt.compare(password,user.password);
+        if(!valid){
+            return res.send('senha incorreta');
+        }
+        req.session.userId=user.indexOf;
+        res.redirect('/dashboard.html');
+        
+
+    }catch(err){
+        console.error(err);
+        res.send('erro no login');
+
+    }
+
+});
+
+app.get('/dashboard',async(req,res)=>{
+ if(!req.session.userId){
+    return res.redirect('/login.html');
+ }
+ res.sendFile(__dirname + '/public/dashborad.html');
+
+});
+
+app.listen(port,()=>{
+    console.log('servidor rodando em http://localhost:${PORT}');
+});
+
