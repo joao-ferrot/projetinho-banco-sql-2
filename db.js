2593e321698cabfbedc480ae60bacbea09de23fc
joao-ferrot <jpedrofsilva663@hotmail.com>
2026-01-29 21:03:40 -0300

    mini alteraçoes

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg');
 const pool=new Pool({
     user:'postgres',
     host:'localhost',
-    database:'auth-db',
+    database:'authdb',
     password:'2980',
     port:5432
 });
